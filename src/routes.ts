38b688207503d0885f5747780ef6ac57b400ed1c
VHall1 <victorhallpsn@hotmail.com>
2021-04-01 22:49:20 -0300

    Add create_user route

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,8 @@
+import userController from './controllers/userController';
 import { Router } from 'express';
 
 const routes = Router();
+
+routes.post('/create_user', userController.create);
 
 export { routes };
