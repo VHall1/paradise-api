345e79dfb579b9ebdf46e7a4ccbfeb948bdd1f64
VHall1 <victorhallpsn@hotmail.com>
2021-04-02 14:14:17 -0300

    Remove unused imports

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,11 +1,4 @@
-import {
-  BaseEntity,
-  Column,
-  Entity,
-  OneToMany,
-  PrimaryColumn,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { BaseEntity, Column, Entity, OneToMany, PrimaryColumn } from 'typeorm';
 import { Character } from './Character';
 
 @Entity()
