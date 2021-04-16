5c0bc4a2b8a52cb5b6a11d1dffc6ad114881d352
VHall1 <victorhallpsn@hotmail.com>
2021-04-16 19:16:46 -0300

    feat: add feature tests to userController

--- src/test/controllers/userController.test.ts
+++ src/test/controllers/userController.test.ts
@@ -1,8 +1,12 @@
 import { expect } from 'chai';
 import { main } from '../../server';
+import { User } from '../../entities/User';
 import request from 'supertest';
 
 describe('userController', () => {
+  const steam = 'steam:1100001052df7c1';
+  const discord = '208343668866678786';
+
   before(async () => {
     await main();
   });
@@ -10,11 +14,55 @@ describe('userController', () => {
   it('should be able to create a user', async () => {
     const res = await request(`http://localhost:${process.env.PORT}`)
       .post('/user/create')
-      .set('content-type', 'application/json')
-      .send({
-        steam: 'steam:1100001052df7c1',
-        discord: '208343668866678786',
-      });
+      .send({ steam, discord });
+
+    expect(res.body.user.steam).to.equal(steam);
+    expect(res.body.user.discord).to.equal(discord);
+
+    expect(res.status).to.equal(200);
+  });
+
+  it('should be able to get a user', async () => {
+    const res = await request(`http://localhost:${process.env.PORT}`).get(
+      `/user/${steam}`
+    );
+
+    expect(res.body.user.steam).to.equal(steam);
+    expect(res.body.user.discord).to.equal(discord);
+
+    expect(res.status).to.equal(200);
+  });
+
+  it('should be able to set whitelisted', async () => {
+    const res = await request(`http://localhost:${process.env.PORT}`)
+      .post('/user/set_whitelisted')
+      .send({ steam, status: true });
+
+    const user = await User.findOneOrFail({ steam });
+
+    expect(user.whitelisted).to.equal(true);
+    expect(res.status).to.equal(200);
+  });
+
+  it('should be able to set banned', async () => {
+    const res = await request(`http://localhost:${process.env.PORT}`)
+      .post('/user/set_banned')
+      .send({ steam, status: true });
+
+    const user = await User.findOneOrFail({ steam });
+
+    expect(user.banned).to.equal(true);
+    expect(res.status).to.equal(200);
+  });
+
+  it('should be able to set priority', async () => {
+    const res = await request(`http://localhost:${process.env.PORT}`)
+      .post('/user/set_priority')
+      .send({ steam, priority: 3 });
+
+    const user = await User.findOneOrFail({ steam });
+
+    expect(user.priority).to.equal(3);
     expect(res.status).to.equal(200);
   });
 });
