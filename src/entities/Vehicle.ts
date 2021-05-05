87b63110423dbd40fb697661a15085edc08a7a05
VHall1 <victorhallpsn@hotmail.com>
2021-05-05 20:33:30 -0300

    Fix vehicle Entity

--- src/entities/Vehicle.ts
+++ src/entities/Vehicle.ts
@@ -1,43 +1,54 @@
+import { GraphQLJSON } from 'graphql-type-json';
+import { Field, Int } from 'type-graphql';
 import {
   BaseEntity,
   Column,
   Entity,
+  JoinColumn,
   ManyToOne,
-  PrimaryGeneratedColumn,
+  PrimaryColumn,
 } from 'typeorm';
 import { Character } from './Character';
 
 @Entity()
 export class Vehicle extends BaseEntity {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @Field(() => Int)
+  @PrimaryColumn()
+  ownerId: number;
 
+  @Field()
   @Column()
   plate: string;
 
+  @Field(() => Int)
   @Column({ default: 100 })
   fuel: number;
 
+  @Field(() => Int)
   @Column({ default: 100 })
   body: number;
 
+  @Field(() => Int)
   @Column({ default: 100 })
   engine: number;
 
+  @Field(() => GraphQLJSON, { nullable: true })
   @Column('simple-array')
   lastCoords: number[];
 
+  @Field(() => GraphQLJSON, { nullable: true })
   @Column('simple-array')
   tires: boolean[];
 
+  @Field(() => GraphQLJSON, { nullable: true })
   @Column('simple-array')
   windows: boolean[];
 
+  @Field(() => GraphQLJSON, { nullable: true })
   @Column('simple-array')
   doors: boolean[];
 
-  @ManyToOne(() => Character, (character) => character.vehicles, {
-    onDelete: 'CASCADE',
-  })
+  @JoinColumn()
+  @ManyToOne(() => Character, { onDelete: 'CASCADE' })
   owner: Character;
 }
